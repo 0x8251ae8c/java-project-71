b7969fbe1cdc4137324b76588d7217205bba9bbe
0x8251ae8c <99046156+0x8251ae8c@users.noreply.github.com>
2024-04-14 00:41:36 +0700

    Add class Parser

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,49 +1,15 @@
 package hexlet.code;
 
 import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
 import java.util.TreeMap;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 public class Differ {
     public static String generate(File filepath1, File filepath2) throws Exception {
         String result = "";
 
-        Path file1 = Paths.get(filepath1.toURI()).toAbsolutePath().normalize();
-        if (!Files.exists(file1)) {
-            throw new Exception("File '" + file1 + "' does not exist");
-        }
-
-        Path file2 = Paths.get(filepath2.toURI()).toAbsolutePath().normalize();
-        if (!Files.exists(file2)) {
-            throw new Exception("File '" + file2 + "' does not exist");
-        }
-
-        Map<String, String> map1 = null;
-        Map<String, String> map2 = null;
-
-        var fileType = getFileExtension(file1);
-
-        switch (fileType) {
-            case "json":
-                ObjectMapper jsonMapper = new ObjectMapper();
-                map1 = jsonMapper.readValue(Files.readString(file1), new TypeReference<>() { });
-                map2 = jsonMapper.readValue(Files.readString(file2), new TypeReference<>() { });
-                break;
-            case "yml":
-                ObjectMapper yamlMapper = new YAMLMapper();
-                map1 = yamlMapper.readValue(Files.readString(file1), new TypeReference<>() { });
-                map2 = yamlMapper.readValue(Files.readString(file2), new TypeReference<>() { });
-                break;
-            default:
-                return null;
-        }
-
+        Map<String, String> map1 = Parser.parse(filepath1);
+        Map<String, String> map2 = Parser.parse(filepath2);
         Map<String, String> sortedMap = new TreeMap<>(map1);
         sortedMap.putAll(map2);
 
@@ -71,9 +37,5 @@ public class Differ {
         result += "}";
 
         return result;
-    }
-
-    private static String getFileExtension(Path file) {
-        return file.getFileName().toString().split("\\.")[1];
     }
 }
---
+++ app/src/main/java/hexlet/code/Parser.java
@@ -0,0 +1,34 @@
+package hexlet.code;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
+public class Parser {
+    public static Map<String, String> parse(File filepath) throws Exception {
+        Path file = Paths.get(filepath.toURI()).toAbsolutePath().normalize();
+        if (!Files.exists(file)) {
+            throw new Exception("File '" + file + "' does not exist");
+        }
+
+        ObjectMapper mapper = getMapper(file);
+
+        return mapper.readValue(Files.readString(file), new TypeReference<>() { });
+    }
+
+    private static ObjectMapper getMapper(Path file) {
+        var fileType = file.getFileName().toString().split("\\.")[1];
+
+        return switch (fileType) {
+            case "json" -> new ObjectMapper();
+            case "yml" -> new YAMLMapper();
+            default -> null;
+        };
+    }
+}
