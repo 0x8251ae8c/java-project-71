387b376e8a4946335242442e42564a93ab01ccb7
0x8251ae8c <99046156+0x8251ae8c@users.noreply.github.com>
2024-04-17 22:12:47 +0700

    Add generateDiff method

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,6 +1,7 @@
 package hexlet.code;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
@@ -15,6 +16,7 @@ public class Differ {
 
         Map<String, Object> map1 = Parser.parse(filepath1);
         Map<String, Object> map2 = Parser.parse(filepath2);
+
         Map<String, Object> sortedMap = new TreeMap<>(map1);
         sortedMap.putAll(map2);
 
@@ -38,5 +40,34 @@ public class Differ {
             }
         }
         return formatter.toString();
+    }
+
+    private static Map<String, String> generateDiff(Map<String, Object> map1, Map<String, Object> map2) {
+        Map<String, Object> map = new HashMap<>(map1);
+        map.putAll(map2);
+
+        Set<String> keysMap1 = map1.keySet();
+        Set<String> keysMap2 = map2.keySet();
+
+        Map<String, String> diff = new TreeMap<>();
+
+        for (String key : map.keySet()) {
+            String valueState;
+            if (!keysMap2.contains(key)) {
+                valueState = "removed";
+            } else if (!keysMap1.contains(key)) {
+                valueState = "added";
+            } else {
+                String value1 = String.valueOf(map1.get(key));
+                String value2 = String.valueOf(map2.get(key));
+                if (value1.equals(value2)) {
+                    valueState = "kept";
+                } else {
+                    valueState = "changed";
+                }
+            }
+            diff.put(key, valueState);
+        }
+        return diff;
     }
 }
