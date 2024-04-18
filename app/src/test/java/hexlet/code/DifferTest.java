094c4c893bc107ea46eb71ebe648caf26c67db37
0x8251ae8c <99046156+0x8251ae8c@users.noreply.github.com>
2024-04-18 21:29:07 +0700

    Add test for plain formatter

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.nio.file.Files;
@@ -9,27 +10,46 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class DifferTest {
     private static File filepath1;
     private static File filepath2;
-    private static File result;
+    private static File differStylish;
+    private static File differPlain;
+
+    @BeforeAll
+    public static void beforeAll() {
+        differStylish = new File("./src/test/resources/differStylish");
+        differPlain = new File("./src/test/resources/differPlain");
+    }
 
     @Test
     public void testDifferJson() throws Exception {
         filepath1 = new File("./src/test/resources/filepath1.json");
         filepath2 = new File("./src/test/resources/filepath2.json");
-        result = new File("./src/test/resources/differStylish");
 
-        String expected = Files.readString(result.toPath());
-        String actual = Differ.generate(filepath1, filepath2);
-        assertEquals(expected, actual);
+        testStylish(filepath1, filepath2);
+        testPlain(filepath1, filepath2);
     }
 
     @Test
     public void testDifferYaml() throws Exception {
         filepath1 = new File("./src/test/resources/filepath1.yml");
         filepath2 = new File("./src/test/resources/filepath2.yml");
-        result = new File("./src/test/resources/differStylish");
 
-        String expected = Files.readString(result.toPath());
-        String actual = Differ.generate(filepath1, filepath2);
+        testStylish(filepath1, filepath2);
+        testPlain(filepath1, filepath2);
+    }
+
+    private void testStylish(File fp1, File fp2) throws Exception {
+        String expected = Files.readString(differStylish.toPath());
+        String actual1 = Differ.generate(fp1, fp2);
+        String actual2 = Differ.generate(fp1, fp2, "stylish");
+
+        assertEquals(expected, actual1);
+        assertEquals(expected, actual2);
+    }
+
+    private void testPlain(File fp1, File fp2) throws Exception {
+        String expected = Files.readString(differPlain.toPath());
+        String actual = Differ.generate(fp1, fp2, "plain");
+
         assertEquals(expected, actual);
     }
 }
