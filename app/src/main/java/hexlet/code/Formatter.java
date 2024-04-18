0d65e41c4da2ca68f66fb0f8ce97edcc5558ee41
0x8251ae8c <99046156+0x8251ae8c@users.noreply.github.com>
2024-04-18 23:34:18 +0700

    Update formatter

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -1,5 +1,7 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import hexlet.code.formatters.Json;
 import hexlet.code.formatters.Plain;
 import hexlet.code.formatters.Stylish;
 
@@ -9,10 +11,11 @@ public class Formatter {
     public static String generate(Map<String, Object> map1,
                                   Map<String, Object> map2,
                                   Map<String, String> diff,
-                                  String format) {
+                                  String format) throws JsonProcessingException {
         return switch (format) {
             case "stylish" -> Stylish.generate(map1, map2, diff);
             case "plain" -> Plain.generate(map1, map2, diff);
+            case "json" -> Json.generate(map1, map2, diff);
             default -> throw new IllegalStateException("Unexpected value: " + format);
         };
     }
