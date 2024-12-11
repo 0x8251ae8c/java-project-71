aef40c756bf44939d17d3cd162dedbc7fa424854
0x8251ae8c <99046156+0x8251ae8c@users.noreply.github.com>
2024-12-12 00:04:39 +0700

    Update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,12 +2,21 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Option;
 
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "0.1",
         description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
+    @Parameters(index = "0", description = "path to first file")
+    private String filepath1;
+    @Parameters(index = "1", description = "path to second file")
+    private String filepath2;
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format = "stylish";
 
     @Override
     public Integer call() {
