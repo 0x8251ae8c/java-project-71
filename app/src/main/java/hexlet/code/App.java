857b82911badf9bb0558c525b91a3a457712ecd9
0x8251ae8c <99046156+0x8251ae8c@users.noreply.github.com>
2024-12-12 21:27:31 +0700

    Update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,10 +1,16 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Option;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "0.1",
@@ -19,8 +25,28 @@ public class App implements Callable<Integer> {
     private String format = "stylish";
 
     @Override
-    public Integer call() {
-        System.out.println("Hello World!");
+    public Integer call() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        Path path1 = Paths.get(filepath1).toAbsolutePath().normalize();
+        Path path2 = Paths.get(filepath2).toAbsolutePath().normalize();
+
+        if (!Files.exists(path1)) {
+            throw new Exception("File '" + path1 + "' does not exist");
+        }
+        if (!Files.exists(path2)) {
+            throw new Exception("File '" + path2 + "' does not exist");
+        }
+
+        String content1 = Files.readString(path1);
+        String content2 = Files.readString(path2);
+
+        var map1 = objectMapper.readValue(content1, new TypeReference<Map<String, String>>(){});
+        var map2 = objectMapper.readValue(content2, new TypeReference<Map<String, String>>(){});
+
+        System.out.println(filepath1 + ": " + map1);
+        System.out.println(filepath2 + ": " + map2);
+
         return 0;
     }
 
