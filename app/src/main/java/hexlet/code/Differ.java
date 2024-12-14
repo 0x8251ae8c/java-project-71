da6ca2f256fec0bbfacdd53365ae63992698796e
0x8251ae8c <99046156+0x8251ae8c@users.noreply.github.com>
2024-12-15 00:38:17 +0700

    Update Differ.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,20 +13,20 @@ import java.util.StringJoiner;
 
 public class Differ {
     public static String generate(String filepath1, String filepath2) throws Exception {
-        var map1 = parseJsonToMap(filepath1);
-        var map2 = parseJsonToMap(filepath2);
+        var map1 = parseToMap(filepath1);
+        var map2 = parseToMap(filepath2);
 
         return compare(map1, map2);
     }
 
     private static String compare(Map<String, String> map1, Map<String, String> map2) {
-        var map = new HashMap<String, String>(map1);
+        var map = new HashMap<>(map1);
         map.putAll(map2);
 
         var sj = new StringJoiner("\n");
 
         map.entrySet().stream()
-                .sorted(Map.Entry.<String, String>comparingByKey())
+                .sorted(Map.Entry.comparingByKey())
                 .forEach(entry -> {
                     var key = entry.getKey();
                     var value = entry.getValue();
@@ -50,11 +51,14 @@ public class Differ {
         return "{\n" + sj + "\n}";
     }
 
-    private static Map<String, String> parseJsonToMap(String filepath) throws Exception {
-        var mapper = new ObjectMapper();
+    private static Map<String, String> parseToMap(String filepath) throws Exception {
+        var fileType = filepath.split("\\.")[1];
+        var mapper = fileType.equals("json") ? new ObjectMapper() : new YAMLMapper();
+
         var content = readFile(filepath);
 
-        return mapper.readValue(content, new TypeReference<Map<String, String>>() { });
+        return mapper.readValue(content, new TypeReference<>() {
+        });
     }
 
     private static String readFile(String filepath) throws Exception {
