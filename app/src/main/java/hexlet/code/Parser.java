8e9393ce0dd45c8d6303052454de803efe7e8de3
0x8251ae8c <99046156+0x8251ae8c@users.noreply.github.com>
2024-12-16 01:24:29 +0700

    Update Parser.java

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -4,46 +4,14 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.Map;
 
 public class Parser {
-    static Map<String, String> parseToMap(String filepath) throws Exception {
-        var fileContent = readFile(filepath);
-        var fileType = filepath.split("\\.")[1];
-
-        Map<String, Object> mapper =  switch (fileType) {
+    static Map<String, Object> parseToMap(String fileContent, String fileType) throws Exception {
+        return switch (fileType) {
             case ("json") -> new ObjectMapper().readValue(fileContent, new TypeReference<>() { });
             case ("yaml"), ("yml") -> new YAMLMapper().readValue(fileContent, new TypeReference<>() { });
-            default -> null;
+            default -> throw new RuntimeException("unknown file type");
         };
-
-        if (mapper == null) {
-            return null;
-        }
-
-        var result = new HashMap<String, String>();
-
-        for (var entry : mapper.entrySet()) {
-            var key = entry.getKey();
-            var value = entry.getValue();
-
-            result.put(key, value == null ? "null" : value.toString());
-        }
-
-        return result;
-    }
-
-    private static String readFile(String filepath) throws Exception {
-        Path path = Paths.get(filepath).toAbsolutePath().normalize();
-
-        if (!Files.exists(path)) {
-            throw new Exception("File '" + path + "' does not exist");
-        }
-
-        return Files.readString(path);
     }
 }
