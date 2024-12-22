ead47ee5839570389332b2e10ff79f5df1b06879
0x8251ae8c <99046156+0x8251ae8c@users.noreply.github.com>
2024-12-22 16:53:43 +0700

    Update DifferTest.java

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,5 +1,8 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -10,6 +13,11 @@ import java.nio.file.Paths;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DifferTest {
+    private static String expectedStylishDiffer;
+    private static String expectedPlainDiffer;
+    private static String expectedJsonDiffer;
+    private static ObjectMapper mapper;
+
     private static Path getFixturePath(String fileName) {
         return Paths.get("src", "test", "resources", "fixtures", fileName)
                 .toAbsolutePath().normalize();
@@ -20,27 +28,35 @@ class DifferTest {
         return Files.readString(path).trim();
     }
 
+    @BeforeAll
+    public static void beforeAll() throws Exception {
+        expectedStylishDiffer = readFixture("differStylish");
+        expectedPlainDiffer = readFixture("differPlain");
+        expectedJsonDiffer = readFixture("differJson");
+        mapper = new ObjectMapper();
+    }
+
     @ParameterizedTest
     @ValueSource(strings = { "json", "yml" })
     public void generate(String extension) throws Exception {
-        var filepath1 = getFixturePath("file1." + extension);
-        var filepath2 = getFixturePath("file2." + extension);
+        var filepath1 = getFixturePath("file1." + extension).toString();
+        var filepath2 = getFixturePath("file2." + extension).toString();
 
-        var expectedStylishDiffer = readFixture("differStylish");
-        var actualStylishDiffer1 = Differ.generate(filepath1.toString(), filepath2.toString(), "stylish");
-        var actualStylishDiffer2 = Differ.generate(filepath1.toString(), filepath2.toString());
+        var actualStylishDiffer1 = Differ.generate(filepath1, filepath2, "stylish");
+        var actualStylishDiffer2 = Differ.generate(filepath1, filepath2);
 
         assertEquals(expectedStylishDiffer, actualStylishDiffer1);
         assertEquals(expectedStylishDiffer, actualStylishDiffer2);
 
-        var expectedPlainDiffer = readFixture("differPlain");
-        var actualPlainDiffer = Differ.generate(filepath1.toString(), filepath2.toString(), "plain");
+        var actualPlainDiffer = Differ.generate(filepath1, filepath2, "plain");
 
         assertEquals(expectedPlainDiffer, actualPlainDiffer);
 
-        var expectedJsonDiffer = readFixture("differJson");
-        var actualJsonDiffer = Differ.generate(filepath1.toString(), filepath2.toString(), "json");
+        var actualJsonDiffer = Differ.generate(filepath1, filepath2, "json");
 
-        assertEquals(expectedJsonDiffer, actualJsonDiffer);
+        var mapExpected = mapper.readValue(expectedJsonDiffer, new TypeReference<>() { });
+        var mapActual = mapper.readValue(actualJsonDiffer, new TypeReference<>() { });
+
+        assertEquals(mapExpected, mapActual);
     }
 }
