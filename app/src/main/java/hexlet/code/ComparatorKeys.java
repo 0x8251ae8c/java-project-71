7b7cb19ac6f7d3fced06514d0c36aeffb5fbead8
0x8251ae8c <99046156+0x8251ae8c@users.noreply.github.com>
2024-12-22 15:36:03 +0700

    Add enums

---
+++ app/src/main/java/hexlet/code/ComparatorKeys.java
@@ -0,0 +1,8 @@
+package hexlet.code;
+
+public enum ComparatorKeys {
+    FIELD,
+    STATUS,
+    OLD_VALUE,
+    NEW_VALUE
+}
---
+++ app/src/main/java/hexlet/code/ComparatorStatuses.java
@@ -0,0 +1,8 @@
+package hexlet.code;
+
+public enum ComparatorStatuses {
+    ADDED,
+    REMOVED,
+    CHANGED,
+    UNCHANGED
+}
